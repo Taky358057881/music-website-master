7f1994941c7e96611b0b72b8c12e8e2351289ae6
Taky <358057881@qq.com>
2022-05-10 22:29:16 +0800

    添加consumer成功

---
+++ src/test/java/com/example/yin/MusicServerApplicationTests.java
@@ -0,0 +1,38 @@
+package com.example.yin;
+
+import com.example.yin.domain.Consumer;
+import com.example.yin.service.ConsumerService;
+import com.example.yin.service.impl.ConsumerServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class MusicServerApplicationTests {
+
+    @Autowired
+    private ConsumerServiceImpl consumerService;
+
+    @Test
+    void contextLoads() {
+        Consumer consumer = new Consumer();
+        consumer.setUsername("Taky");
+        consumer.setPassword("123");
+        consumer.setSex(new Byte("0"));
+        consumer.setPhoneNum("19966796664");
+        consumer.setEmail("358057881@qq.com");
+        consumer.setBirth(new Date());
+        consumer.setIntroduction("");
+        consumer.setLocation("");
+        consumer.setAvator("/img/avatorImages/user.jpg");
+        consumer.setCreateTime(new Date());
+        consumer.setUpdateTime(new Date());
+        consumerService.addUser(consumer);
+    }
+
+}
